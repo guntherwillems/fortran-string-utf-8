ebaff9d900214b3ab5210a5c3edd816220803ed5
Gunther Willems <136173143+guntherwillems@users.noreply.github.com>
2024-11-14 21:49:50 +0100

    Batch files check bin folder

--- run-example.bat
+++ run-example.bat
@@ -1,6 +1,7 @@
 @echo off
 SET progr=example
 SET module1=string_utf_8
+IF NOT EXIST bin md bin
 gfortran -c src\%module1%.f90 -J bin -o bin\%module1%.o
 gfortran -o bin\%progr% app\%progr%.f90 bin\%module1%.o -I src -I bin
 REM Set the active console code page to UTF-8
--- run-test.bat
+++ run-test.bat
@@ -1,6 +1,7 @@
 @echo off
 SET progr=tests
 SET module1=string_utf_8
+IF NOT EXIST bin md bin
 gfortran -c src\%module1%.f90 -J bin -o bin\%module1%.o
 gfortran -o bin\%progr% test\%progr%.f90 bin\%module1%.o -I src -I bin
 bin\%progr%
